8739ca7126801d33a720a95046d583ca86a802dc
ChakramCPP <chakrapani.nallam@gmail.com>
2020-10-06 14:34:40 +0530

    basic components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SampleComponent } from './components/chakri/sample/sample.component';
 import { Sample2Component } from './components/chakri/sample2/sample2.component';
+import { KanbanComponent } from './components/chakri/kanban/kanban.component';
+import { ChatComponent } from './components/chakri/chat/chat.component';
+import { PollComponent } from './components/chakri/poll/poll.component';
+import { SurveyComponent } from './components/chakri/survey/survey.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SampleComponent,
-    Sample2Component
+    Sample2Component,
+    KanbanComponent,
+    ChatComponent,
+    PollComponent,
+    SurveyComponent
   ],
   imports: [
     BrowserModule,
